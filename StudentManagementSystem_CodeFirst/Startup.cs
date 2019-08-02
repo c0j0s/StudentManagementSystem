3510a826dd588dc49f85b0d4a6685cbb2ab16e84
CJS <c.junsheng@hotmail.com>
2019-08-02 13:47:25 +0800

    Create Database

--- StudentManagementSystem_CodeFirst/Startup.cs
+++ StudentManagementSystem_CodeFirst/Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using StudentManagementSystem_CodeFirst.Models;
 
 namespace StudentManagementSystem_CodeFirst
 {
@@ -33,6 +35,12 @@ namespace StudentManagementSystem_CodeFirst
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            var connectionString = Configuration.GetConnectionString("StudentManagementSystemDatabase");
+
+            services.AddDbContext<StudentManagementSystemContext>
+                (options => options.UseSqlServer(connectionString));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
