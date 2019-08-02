cad93e5f4027df9cbce9bba0091541fa8a4a6582
CJS <c.junsheng@hotmail.com>
2019-08-02 11:22:12 +0800

    Define Constrains

--- StudentManagementSystem_CodeFirst/Models/Student.cs
+++ StudentManagementSystem_CodeFirst/Models/Student.cs
@@ -2,15 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace StudentManagementSystem_CodeFirst.Models
 {
     public class Student {
+        //Since our primary key does not end with "ID"
+        [Key]
+        [StringLength(7)]
         public string AdminNo { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
+        //We only want to store date
+        [DataType(DataType.Date)]
+        //Display name
+        [Display(Name = "Date of Birth")]
         public DateTime Dob { get; set; }
+
+        //Since we only want "M" or "F"
+        [StringLength(1)]
         public string Gender { get; set; }
+
+        [Required]
+        // A regular expression for Singapore telephone number
+        [RegularExpression(@"(6|8|9)\d{7}",
+            ErrorMessage = "Invalid Phone Number!")]
+        [Display(Name = "Handphone")]
         public string ContactNumber { get; set; }
+
 
         public Address Address { get; set; }
 
