763b84ff856d8e34c0646bac98c4c0d8e7ffbeb2
CJS <c.junsheng@hotmail.com>
2019-08-05 11:10:44 +0800

    Add methods in controller

--- StudentManagementSystem_CodeFirst/Controllers/HomeController.cs
+++ StudentManagementSystem_CodeFirst/Controllers/HomeController.cs
@@ -10,9 +10,22 @@ namespace StudentManagementSystem_CodeFirst.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        #region define context
+        private readonly StudentManagementSystemContext _context;
+
+        public HomeController(StudentManagementSystemContext context)
         {
-            return View();
+            _context = context;
+        }
+        #endregion
+
+        #region Routing Methods
+
+        //Error handling page
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
         public IActionResult Privacy()
@@ -20,10 +33,73 @@ namespace StudentManagementSystem_CodeFirst.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public async Task<IActionResult> Index()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Details(string id)
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            return View();
+        }
+        #endregion
+
+        #region Form handling Methods
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(
+            [Bind("AdminNo,Name,Dob,Gender,ContactNumber,DiplomaId,Address")] Student student)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(
+            string id,
+            [Bind("AdminNo,Name,Dob,Gender,ContactNumber,DiplomaId,Address")] Student student)
+        {
+            return View(student);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(string id)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddModules(string id, string[] StudentModules)
+        {
+            return RedirectToAction(nameof(Details), new { id = id });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteModules(string id, string[] StudentModules)
+        {
+            return RedirectToAction(nameof(Details), new { id = id });
+        }
+        #endregion
+
+        #region Other helper methods
+        private bool StudentExists(string id)
+        {
+            return _context.Students.Any(e => e.AdminNo == id);
+        }
+        #endregion
     }
 }
+
